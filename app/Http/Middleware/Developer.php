7f5ab9a91b6609eb39a4b9bb2a587b362b78b1bc
KokoFibo <kokonacci@gmail.com>
2024-09-22 09:15:36 +0700

    tambah rubah id 1

--- app/Http/Middleware/Developer.php
+++ app/Http/Middleware/Developer.php
@@ -6,6 +6,8 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Http;
+
 
 class Developer
 {
