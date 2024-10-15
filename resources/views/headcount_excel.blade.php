ada572742640e87e85bab6ea441a2082e5d150b8
KokoFibo <kokonacci@gmail.com>
2024-10-15 13:05:27 +0700

    rubah ke placement

--- resources/views/headcount_excel.blade.php
+++ resources/views/headcount_excel.blade.php
@@ -7,6 +7,12 @@
             <th>YCME</th>
             <th>YSM</th>
             <th>YAM</th>
+            <th>YEV</th>
+            <th>YEV SMOOT</th>
+            <th>YEV OFFERO</th>
+            <th>YEV SUNRA</th>
+            <th>YEV AIMA</th>
+            <th>YEV ELEKTRONIK</th>
         </tr>
     </thead>
     <tbody>
@@ -16,6 +22,12 @@
             <td>{{ $ycme_hr }}</td>
             <td>{{ $ysm_hr }}</td>
             <td>{{ $yam_hr }}</td>
+            <td>{{ $yev_hr }}</td>
+            <td>{{ $yevsmoot_hr }}</td>
+            <td>{{ $yevoffero_hr }}</td>
+            <td>{{ $yevsunra_hr }}</td>
+            <td>{{ $yevaima_hr }}</td>
+            <td>{{ $yevelektronik_hr }}</td>
 
         </tr>
         <tr>
@@ -24,6 +36,12 @@
             <td>{{ $ycme_finance }}</td>
             <td>{{ $ysm_finance }}</td>
             <td>{{ $yam_finance }}</td>
+            <td>{{ $yev_finance }}</td>
+            <td>{{ $yevsmoot_finance }}</td>
+            <td>{{ $yevoffero_finance }}</td>
+            <td>{{ $yevsunra_finance }}</td>
+            <td>{{ $yevaima_finance }}</td>
+            <td>{{ $yevelektronik_finance }}</td>
 
         </tr>
         <tr>
@@ -32,6 +50,12 @@
             <td>{{ $ycme_procurement }}</td>
             <td>{{ $ysm_procurement }}</td>
             <td>{{ $yam_procurement }}</td>
+            <td>{{ $yev_procurement }}</td>
+            <td>{{ $yevsmoot_procurement }}</td>
+            <td>{{ $yevoffero_procurement }}</td>
+            <td>{{ $yevsunra_procurement }}</td>
+            <td>{{ $yevaima_procurement }}</td>
+            <td>{{ $yevelektronik_procurement }}</td>
 
         </tr>
         <tr>
@@ -40,6 +64,13 @@
             <td>{{ $ycme_ga }}</td>
             <td>{{ $ysm_ga }}</td>
             <td>{{ $yam_ga }}</td>
+            <td>{{ $yev_ga }}</td>
+            <td>{{ $yevsmoot_ga }}</td>
+            <td>{{ $yevoffero_ga }}</td>
+            <td>{{ $yevsunra_ga }}</td>
+            <td>{{ $yevaima_ga }}</td>
+            <td>{{ $yevelektronik_ga }}</td>
+
 
         </tr>
         <tr>
@@ -48,6 +79,12 @@
             <td>{{ $ycme_legal }}</td>
             <td>{{ $ysm_legal }}</td>
             <td>{{ $yam_legal }}</td>
+            <td>{{ $yev_legal }}</td>
+            <td>{{ $yevsmoot_legal }}</td>
+            <td>{{ $yevoffero_legal }}</td>
+            <td>{{ $yevsunra_legal }}</td>
+            <td>{{ $yevaima_legal }}</td>
+            <td>{{ $yevelektronik_legal }}</td>
 
         </tr>
         <tr>
@@ -56,6 +93,12 @@
             <td>{{ $ycme_exim }}</td>
             <td>{{ $ysm_exim }}</td>
             <td>{{ $yam_exim }}</td>
+            <td>{{ $yev_exim }}</td>
+            <td>{{ $yevsmoot_exim }}</td>
+            <td>{{ $yevoffero_exim }}</td>
+            <td>{{ $yevsunra_exim }}</td>
+            <td>{{ $yevaima_exim }}</td>
+            <td>{{ $yevelektronik_exim }}</td>
 
         </tr>
 
@@ -65,6 +108,12 @@
             <td>{{ $ycme_me }}</td>
             <td>{{ $ysm_me }}</td>
             <td>{{ $yam_me }}</td>
+            <td>{{ $yev_me }}</td>
+            <td>{{ $yevsmoot_me }}</td>
+            <td>{{ $yevoffero_me }}</td>
+            <td>{{ $yevsunra_me }}</td>
+            <td>{{ $yevaima_me }}</td>
+            <td>{{ $yevelektronik_me }}</td>
 
         </tr>
         <tr>
@@ -73,6 +122,12 @@
             <td>{{ $ycme_bd }}</td>
             <td>{{ $ysm_bd }}</td>
             <td>{{ $yam_bd }}</td>
+            <td>{{ $yev_bd }}</td>
+            <td>{{ $yevsmoot_bd }}</td>
+            <td>{{ $yevoffero_bd }}</td>
+            <td>{{ $yevsunra_bd }}</td>
+            <td>{{ $yevaima_bd }}</td>
+            <td>{{ $yevelektronik_bd }}</td>
 
         </tr>
         <tr>
@@ -81,6 +136,12 @@
             <td>{{ $ycme_qc }}</td>
             <td>{{ $ysm_qc }}</td>
             <td>{{ $yam_qc }}</td>
+            <td>{{ $yev_qc }}</td>
+            <td>{{ $yevsmoot_qc }}</td>
+            <td>{{ $yevoffero_qc }}</td>
+            <td>{{ $yevsunra_qc }}</td>
+            <td>{{ $yevaima_qc }}</td>
+            <td>{{ $yevelektronik_qc }}</td>
 
         </tr>
         <tr>
@@ -89,6 +150,12 @@
             <td>{{ $ycme_production }}</td>
             <td>{{ $ysm_production }}</td>
             <td>{{ $yam_production }}</td>
+            <td>{{ $yev_production }}</td>
+            <td>{{ $yevsmoot_production }}</td>
+            <td>{{ $yevoffero_production }}</td>
+            <td>{{ $yevsunra_production }}</td>
+            <td>{{ $yevaima_production }}</td>
+            <td>{{ $yevelektronik_production }}</td>
 
         </tr>
         <tr>
@@ -97,7 +164,12 @@
             <td>{{ $ycme_warehouse }}</td>
             <td>{{ $ysm_warehouse }}</td>
             <td>{{ $yam_warehouse }}</td>
-
+            <td>{{ $yev_warehouse }}</td>
+            <td>{{ $yevsmoot_warehouse }}</td>
+            <td>{{ $yevoffero_warehouse }}</td>
+            <td>{{ $yevsunra_warehouse }}</td>
+            <td>{{ $yevaima_warehouse }}</td>
+            <td>{{ $yevelektronik_warehouse }}</td>
         </tr>
 
         <tr>
@@ -106,15 +178,26 @@
             <td>{{ $ycme_bod }}</td>
             <td>{{ $ysm_bod }}</td>
             <td>{{ $yam_bod }}</td>
+            <td>{{ $yev_bod }}</td>
+            <td>{{ $yevsmoot_bod }}</td>
+            <td>{{ $yevoffero_bod }}</td>
+            <td>{{ $yevsunra_bod }}</td>
+            <td>{{ $yevaima_bod }}</td>
+            <td>{{ $yevelektronik_bod }}</td>
 
         </tr>
         <tr>
             <th>Total</th>
-            <th>{{ $yig_total }}</th>
-            <th>{{ $ycme_total }}</th>
-            <th>{{ $ysm_total }}</th>
-            <th>{{ $yam_total }}</th>
-
+            <td>{{ $yig_total }}</td>
+            <td>{{ $ycme_total }}</td>
+            <td>{{ $ysm_total }}</td>
+            <td>{{ $yam_total }}</td>
+            <td>{{ $yev_total }}</td>
+            <td>{{ $yevsmoot_total }}</td>
+            <td>{{ $yevoffero_total }}</td>
+            <td>{{ $yevsunra_total }}</td>
+            <td>{{ $yevaima_total }}</td>
+            <td>{{ $yevelektronik_total }}</td>
         </tr>
         {{-- <tr>
             <th>Total</th>
