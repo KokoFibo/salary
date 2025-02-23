908ed907539663fc3ed95c3a405bb3558e3c7016
KokoFibo <kokonacci@gmail.com>
2025-02-23 10:07:13 +0700

    tambah fitur rubah id

--- app/Livewire/ChangeID.php
+++ app/Livewire/ChangeID.php
@@ -9,21 +9,38 @@ use Livewire\Component;
 class ChangeID extends Component
 {
     public $idLama, $idBaru, $namaIdLama, $namaIdBaru;
-    public $is_available = true;
+    public $is_updateable_idBaru = false;
+    public $is_updateable_idLama = false;
+    public $isupdateable = false;
+    public $idLamaError, $idBaruError, $idBarumessage;
+
+    public function clear()
+    {
+        $this->isupdateable = false;
+        $this->is_updateable_idBaru = false;
+        $this->is_updateable_idLama = false;
+        $this->idBaru = '';
+        $this->idLama = '';
+        $this->idLamaError = '';
+        $this->idBaruError = '';
+        $this->idBarumessage = '';
+        $this->namaIdLama = '';
+        $this->namaIdBaru = '';
+    }
+
 
     public function rubah()
     {
-        if ($this->idBaru == '' || $this->idLama == '') $this->is_available = false;
-
-        if ($this->is_available) {
+        if ($this->isupdateable && $this->is_updateable_idBaru && $this->is_updateable_idLama) {
             try {
                 $karyawan = Karyawan::where('id_karyawan', $this->idLama)->first();
                 $karyawan->id_karyawan = $this->idBaru;
                 $karyawan->save();
-                dd($karyawan->id_karyawan);
                 $user = User::where('username', $this->idLama)->first();
                 $user->username = $this->idBaru;
                 $user->save();
+                $this->clear();
+
                 $this->dispatch(
                     'message',
                     type: 'success',
@@ -39,9 +56,6 @@ class ChangeID extends Component
                 );
             }
         }
-        // else {
-        //     dd('tidak boleh ganti');
-        // }
     }
 
     public function cancel()
@@ -51,19 +65,60 @@ class ChangeID extends Component
 
     public function updatedIdLama()
     {
+        $this->is_updateable_idLama = true;
+        if ($this->idLama == '') {
+            $this->is_updateable_idLama = false;
+            return;
+        }
         $data = Karyawan::where('id_karyawan', $this->idLama)->first();
-
-        // Pastikan jika data ditemukan, gunakan nama dari database, jika tidak beri default.
-        $this->namaIdLama = $data ? $data->nama : 'Nama Tidak Ditemukan';
-
-        // Pastikan hanya mengakses property 'nama' jika data tidak null
-        $this->is_available = $data ? false : true;
+        if (!$data) {
+            $this->is_updateable_idLama = false;
+            $this->idLamaError = 'ID Tidak Ditemukan';
+            $this->isupdateable = false;
+            return;
+        } else {
+            $this->is_updateable_idLama = true;
+            $this->idLamaError = '';
+            $this->namaIdLama = $data->nama;
+        }
+        $this->isupdateable = false;
+        if ($this->is_updateable_idLama && $this->is_updateable_idBaru) {
+            $this->isupdateable = true;
+        }
     }
     public function updatedIdBaru()
     {
+        $this->is_updateable_idBaru = true;
+        $this->idBarumessage = '';
+
+        if ($this->idBaru == '') {
+            $this->is_updateable_idBaru = false;
+            $this->isupdateable = false;
+
+            return;
+        }
+
         $data = Karyawan::where('id_karyawan', $this->idBaru)->first();
-        $this->namaIdBaru = $data ? $data->nama : '';
-        $this->is_available = $data ? false : true;
+        if ($data) {
+            $this->is_updateable_idBaru = false;
+            $this->isupdateable = false;
+
+            $this->idBaruError = 'ID ini sudah terdaftar';
+            return;
+        }
+        $dataUser = User::where('username', $this->idBaru)->first();
+        if ($data) {
+            $this->is_updateable_idBaru = false;
+            $this->idBaruError = 'ID ini sudah terdaftar pada database user';
+            $this->isupdateable = false;
+
+            return;
+        }
+        $this->idBaruError = '';
+        $this->idBarumessage = 'ID ini Available, silakan dirubah';
+        if ($this->is_updateable_idLama && $this->is_updateable_idBaru) {
+            $this->isupdateable = true;
+        }
     }
     public function render()
     {
--- resources/views/livewire/change-i-d.blade.php
+++ resources/views/livewire/change-i-d.blade.php
@@ -8,6 +8,7 @@
                 <div class="mb-3">
                     <label for="ID" class="form-label">ID yang akan diganti -> {{ $namaIdLama }}</label>
                     <input type="text" class="form-control" id="ID" wire:model.live='idLama'>
+                    <p class='text-red'>{{ $idLamaError }}</p>
                 </div>
                 <div class="mb-3">
                     <label for="ID2" class="form-label">ID yang diinginkan</label>
@@ -16,11 +17,16 @@
                         @if ($namaIdBaru != '')
                             <p class='text-red'> {{ $namaIdBaru }} sudah ada dalam database</p>
                         @else
-                            <p class='text-blue'>Id ini Available</p>
+                            @if ($idBaruError)
+                                <p class='text-red'>{{ $idBaruError }}</p>
+                            @else
+                                <p class='text-blue'>{{ $idBarumessage }}</p>
+                            @endif
                         @endif
                     @endif
                 </div>
-                <button class="btn btn-success" wire:click='rubah'>Rubah</button>
+                <button class="btn btn-success" wire:click='rubah' wire:confirm="Yakin mau di rubah?"
+                    {{ $isupdateable ? '' : 'disabled' }}>Rubah</button>
                 <button class="btn btn-dark" wire:click='cancel'>Cancel</button>
             </div>
         </div>
