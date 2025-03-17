3307d36f66e6e653eb5d0770df36f87197cce9f4
KokoFibo <kokonacci@gmail.com>
2025-03-17 07:21:44 +0700

    rubah login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
 
                             <div class="row mb-3">
                                 <label for="email"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('No. ID') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('No ID') }}</label>
 
                                 <div class="col-md-6">
 
