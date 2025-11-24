8371ccbf40b54a4fd03e88bcb81e6fa04b7b4f0f
KokoFibo <kokonacci@gmail.com>
2025-11-24 10:20:34 +0700

    tambah directorate

--- resources/views/livewire/karyawan-noscan.blade.php
+++ resources/views/livewire/karyawan-noscan.blade.php
@@ -4,7 +4,7 @@
 
             <h4>
                 Karyawan (Perbulan) No Scan — Total:
-                <span class="text-danger">{{ $total_karyawan_noscan }} Orang</span>
+                <span class="text-danger">{{ $total_karyawan_noscan }}</span>
                 <span class=" ms-2">{{ $periodeNoscan }}</span>
             </h4>
 
@@ -45,6 +45,9 @@
                             <th>#</th>
                             <th>ID Karyawan</th>
                             <th>Nama</th>
+                            <th>Company</th>
+                            <th>Directorate</th>
+                            <th>Department</th>
                             <th>Jabatan</th>
                             <th>Status</th>
                             <th>Company</th>
@@ -57,6 +60,9 @@
                                 <td>{{ $key + 1 }}</td>
                                 <td>{{ $dtr->user_id }}</td>
                                 <td>{{ $dtr->nama }}</td>
+                                <td>{{ nama_company($dtr->company_id) }}</td>
+                                <td>{{ nama_placement($dtr->placement_id) }}</td>
+                                <td>{{ nama_department($dtr->department_id) }}</td>
                                 <td>{{ nama_jabatan($dtr->jabatan_id) }}</td>
                                 <td>{{ $dtr->status_karyawan }}</td>
                                 <td>{{ nama_company($dtr->company_id) }}</td>
